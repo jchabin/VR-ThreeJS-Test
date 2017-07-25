225e1b86b5ce962121e6a585bd11a658b9740ccc
jchabin <jchabin@cherrycreekschools.org>
2017-07-25 17:37:40 -0600

    Update script.js

--- script.js
+++ script.js
@@ -15,48 +15,43 @@ camera = new THREE.PerspectiveCamera(
 	0.01,
 	3000
 );
-camera.position.set(0, 0, 10);
 scene.add(camera);
-
-var light = new THREE.PointLight(0xffffff, 1, 100);
-light.position.set(3, 4, 1);
-scene.add(light);
-
-var plane = new THREE.Mesh(
-	new THREE.PlaneBufferGeometry(100, 100),
-	new THREE.MeshStandardMaterial()
-);
-plane.position.set(0, -10, 0);
-plane.rotation.set(-Math.PI / 2, 0, 0);
-scene.add(plane);
-
+	
+const BOXES = 7;
+	
 var mobile = navigator.userAgent.match("Mobile")!=null||navigator.userAgent.match("Linux;")!=null;
 if(mobile)
 	controls = new THREE.DeviceOrientationControls(camera);
-else
-	controls = new THREE.OrbitControls(camera);
-
-const BOXES = 100;
-for(var i = 0; i < BOXES; i++){
-	var height = Math.random() * 10;
-	var a = new THREE.Mesh(
-		new THREE.BoxBufferGeometry(1, height, 1),
-		new THREE.MeshStandardMaterial()
-	);
-	a.position.set((Math.random()<0.5?1:-1) * (Math.random() * 10 + 10), height / 2 - 10, (Math.random()<0.5?1:-1) * (Math.random() * 10 + 10));
-	scene.add(a);
+else{
+	controls = new THREE.OrbitControls(camera, element);
+	camera.position.set(0, BOXES * 2.5, 0);
 }
 
 var effect = new THREE.StereoEffect(renderer);
 effect.eyeSeparation = 10;
-effect.setSize( window.innerWidth, window.innerHeight );
+effect.setSize(window.innerWidth, window.innerHeight);
 
-camera.rotation.set(0, 0, 0);
+for(var x = 0; x < BOXES; x++)
+	for(var y = 0; y < BOXES; y++)
+		for(var z = 0; z < BOXES; z++){
+			var a = new THREE.Mesh(
+				new THREE.BoxBufferGeometry(5, 5, 5),
+				new THREE.MeshStandardMaterial()
+			);
+			a.position.set((x - BOXES / 2) * 10 + 5, (y - BOXES / 2) * 10 + 5, (z - BOXES / 2) * 10 + 5);
+			if(!(a.position.x == 0 && a.position.y == 0 && a.position.z == 0))
+				scene.add(a);
+		}
+		
+var light = new THREE.PointLight();
+scene.add(light);
 
 var x = 0;
+
 function render() {
 	requestAnimationFrame(render);
 	controls.update();
+	light.position.set(20 * Math.sin(x), 20 * Math.cos(x), 20 * Math.tan(x));
 	x += 0.01;
 	effect.render(scene, camera);
 }
