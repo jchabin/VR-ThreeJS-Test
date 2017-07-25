a5777d26ce4f1e5994f5ef9d228b6ce10abdd755
jchabin <jchabin@cherrycreekschools.org>
2017-07-25 17:03:43 -0600

    Update script.js

--- script.js
+++ script.js
@@ -22,21 +22,30 @@ var light = new THREE.PointLight(0xffffff, 1, 100);
 light.position.set(3, 4, 1);
 scene.add(light);
 
-var cube = new THREE.Mesh(
-	new THREE.BoxBufferGeometry(1, 1, 1),
-	new THREE.MeshPhongMaterial()
-);
-scene.add(cube);
-
 var plane = new THREE.Mesh(
 	new THREE.PlaneBufferGeometry(100, 100),
 	new THREE.MeshStandardMaterial()
 );
 plane.position.set(0, -10, 0);
-plane.rotation.set(-90, 0, 0);
+plane.rotation.set(-Math.PI / 2, 0, 0);
 scene.add(plane);
 
-controls = new THREE.DeviceOrientationControls(camera);
+var mobile = navigator.userAgent.match("Mobile")!=null||navigator.userAgent.match("Linux;")!=null;
+if(mobile)
+	controls = new THREE.DeviceOrientationControls(camera);
+else
+	controls = new THREE.OrbitControls(camera);
+
+const BOXES = 100;
+for(var i = 0; i < BOXES; i++){
+	var height = Math.random() * 10;
+	var a = new THREE.Mesh(
+		new THREE.BoxBufferGeometry(1, height, 1),
+		new THREE.MeshStandardMaterial()
+	);
+	a.position.set((Math.random()<0.5?1:-1) * (Math.random() * 10 + 10), height / 2 - 10, (Math.random()<0.5?1:-1) * (Math.random() * 10 + 10));
+	scene.add(a);
+}
 
 var effect = new THREE.StereoEffect(renderer);
 effect.eyeSeparation = 10;
@@ -48,7 +57,6 @@ var x = 0;
 function render() {
 	requestAnimationFrame(render);
 	controls.update();
-	cube.rotation.y = x;
 	x += 0.01;
 	effect.render(scene, camera);
 }
