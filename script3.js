6187d5ad974df713eb2c996ab5116baee81c3192
jchabin <jchabin@cherrycreekschools.org>
2017-07-26 15:19:43 -0600

    Update script3.js

--- script3.js
+++ script3.js
@@ -43,7 +43,7 @@ var x = 0;
 function render() {
 	requestAnimationFrame(render);
 	controls.update();
-c.rotation.set(0, x, -Math.PI / 2);
+c.rotation.set(x, 0, -Math.PI / 2);
 	x += 0.01;
 	effect.render(scene, camera);
 }
