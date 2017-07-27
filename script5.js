37a35a592f5aebdb7c8838fdf36728f7372a4f05
jchabin <jchabin@cherrycreekschools.org>
2017-07-27 06:41:11 -0600

    Update script5.js

--- script5.js
+++ script5.js
@@ -1,1 +1,49 @@
+var camera, renderer, scene;
 
+scene = new THREE.Scene();
+renderer = new THREE.WebGLRenderer();
+renderer.setSize(window.innerWidth, window.innerHeight);
+
+var element = renderer.domElement;
+document.body.appendChild(element);
+
+scene.background = new THREE.Color(0x000000);
+scene.fog = new THREE.Fog(0x000000, 100, 200);
+
+camera = new THREE.PerspectiveCamera(
+	90,
+	window.innerWidth / window.innerHeight,
+	0.01,
+	3000
+);
+camera.position.set(0, 5, 0);
+scene.add(camera);
+	
+var mobile = navigator.userAgent.match("Mobile")!=null||navigator.userAgent.match("Linux;")!=null;
+if(mobile)
+	controls = new THREE.DeviceOrientationControls(camera);
+else{
+	controls = new THREE.OrbitControls(camera, element);
+
+}
+
+var effect = new THREE.StereoEffect(renderer);
+effect.eyeSeparation = 10;
+effect.setSize(window.innerWidth, window.innerHeight);
+
+var light = new THREE.PointLight();
+scene.add(light);
+light.position.set(0, 10, 0);
+
+var plane = new THREE.Mesh(new THREE.PlaneBufferGeometry(10, 10), new THREE.MeshPhongMaterial());
+plane.rotation.set(-Math.PI / 2, 0, 0);
+plane.position.set(0, 0, 0);
+scene.add(plane);
+var x = 0;
+
+function render() {
+	requestAnimationFrame(render);
+	controls.update();
+	x += 0.01;
+effect.render(scene, camera);
+}render();
