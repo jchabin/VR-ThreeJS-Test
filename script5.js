e1afe81a58e20261ef705ced7a5beb7b108b0feb
jchabin <jchabin@cherrycreekschools.org>
2017-07-27 10:05:42 -0600

    Update script5.js

--- script5.js
+++ script5.js
@@ -1,3 +1,6 @@
+const BALLS = 10;
+var index = 0;
+
 var camera, renderer, scene;
 
 scene = new THREE.Scene();
@@ -55,41 +58,62 @@ ball.position.set(0, 0, 0);
 ball.xv = Math.random() - 0.5;
 ball.yv = Math.random() - 0.5;
 ball.zv = Math.random() - 0.5;
-scene.add(ball);
+//scene.add(ball);
+
+var balls = [];
 
 function render() {
 	requestAnimationFrame(render);
 	controls.update();
-	ball.position.x += ball.xv;
-	ball.position.y += ball.yv;
-	ball.position.z += ball.zv;
-ball.yv -= 0.01;
-if(ball.position.y < -4){
-ball.position.y = -4;
-ball.yv *= -0.3;
+for(var i = 0; i < balls.length; i++){
+	balls[i].position.x += balls[i].xv;
+	balls[i].position.y += balls[i].yv;
+	balls[i].position.z += balls[i].zv;
+balls[i].yv -= 0.01;
+if(balls[i].position.y < -4){
+balls[i].position.y = -4;
+balls[i].yv *= -0.3;
 }
-if(ball.position.y > 4){
-ball.position.y = 4;
-ball.yv *= -0.3;
+if(balls[i].position.y > 4){
+balls[i].position.y = 4;
+balls[i].yv *= -0.3;
 }
-if(ball.position.x < -4){
-ball.position.x = -4;
-ball.xv *= -0.3;
+if(balls[i].position.x < -4){
+balls[i].position.x = -4;
+balls[i].xv *= -0.3;
 }
-if(ball.position.x > 4){
-ball.position.x = 4;
-ball.xv *= -0.3;
+if(balls[i].position.x > 4){
+balls[i].position.x = 4;
+balls[i].xv *= -0.3;
 }
-if(ball.position.z < -4){
-ball.position.z = -4;
-ball.zv *= -0.3;
+if(balls[i].position.z < -4){
+balls[i].position.z = -4;
+balls[i].zv *= -0.3;
 }
-if(ball.position.z > 4){
-ball.position.z = 4;
-ball.zv *= -0.3;
+if(balls[i].position.z > 4){
+balls[i].position.z = 4;
+balls[i].zv *= -0.3;
 }
-ball.xv *= 0.98;
-ball.yv *= 0.98;
-ball.zv *= 0.98;
+balls[i].xv *= 0.98;
+balls[i].yv *= 0.98;
+balls[i].zv *= 0.98;
+}
 effect.render(scene, camera);
 }render();
+
+window.onclick = function(e){
+var b;
+if(balls.length < BALLS)
+b = ball.clone();
+else
+b = balls[i % BALLS];
+b.position.set(0, 0, 0);
+b.xv = Math.sin(camera.rotation.y) * Math.cos(camera.rotation.x);
+b.yv = Math.sin(camera.rotation.x);
+b.zv = Math.cos(camera.rotation.y) * Math.cos(camera.rotation.x);
+if(balls.length < BALLS){
+balls.push(b);
+scene.add(b);
+}else
+index++;
+}
