453ea3206d498ce657233df8ab70cba907220f77
jchabin <jchabin@cherrycreekschools.org>
2017-07-27 06:43:28 -0600

    Update script5.js

--- script5.js
+++ script5.js
@@ -35,7 +35,7 @@ var light = new THREE.PointLight();
 scene.add(light);
 light.position.set(0, 10, 0);
 
-var plane = new THREE.Mesh(new THREE.PlaneBufferGeometry(10, 10), new THREE.MeshPhongMaterial());
+var plane = new THREE.Mesh(new THREE.PlaneBufferGeometry(10, 10), new THREE.MeshBasicMaterial({color: 0xeeeeee}));
 plane.rotation.set(-Math.PI / 2, 0, 0);
 plane.position.set(0, 0, 0);
 scene.add(plane);
