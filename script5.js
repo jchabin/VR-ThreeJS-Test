72731192274d9ec34a2a806cc4c63cc1f7e4eb48
jchabin <jchabin@cherrycreekschools.org>
2017-07-27 15:49:32 -0600

    Update script5.js

--- script5.js
+++ script5.js
@@ -98,9 +98,9 @@ balls[i].zv *= -0.3;
 
 for(var n = 0; n < balls.length; n++)
 if(n != i && balls[n].position.distanceTo(balls[i].position) < 2){
-balls[i].xv += 1 / (balls[i].position.x - balls[n].position.x) * BOUNCE;
-balls[i].yv += 1 / (balls[i].position.y - balls[n].position.y) * BOUNCE;
-balls[i].zv += 1 / (balls[i].position.z - balls[n].position.z) * BOUNCE;
+balls[i].xv += (balls[i].position.x - balls[n].position.x) * BOUNCE;
+balls[i].yv += (balls[i].position.y - balls[n].position.y) * BOUNCE;
+balls[i].zv += (balls[i].position.z - balls[n].position.z) * BOUNCE;
 }
 
 balls[i].xv *= 0.98;
