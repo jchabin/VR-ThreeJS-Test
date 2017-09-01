bef79871c54fdfbdec2e202e050fdff9e96c5621
jchabin <jchabin@cherrycreekschools.org>
2017-09-01 10:43:11 -0600

    Update script.js

--- editor/script.js
+++ editor/script.js
@@ -29,3 +29,17 @@ if(mobile){
 function run(){
 	database.ref("/text").set(text.value.replace("\n", " "));
 }
+
+//Shamelessly stolen from https://stackoverflow.com/questions/6637341/use-tab-to-indent-in-textarea
+var textareas = document.getElementsByTagName('textarea');
+var count = textareas.length;
+for(var i=0;i<count;i++){
+    textareas[i].onkeydown = function(e){
+        if(e.keyCode==9 || e.which==9){
+            e.preventDefault();
+            var s = this.selectionStart;
+            this.value = this.value.substring(0,this.selectionStart) + "\t" + this.value.substring(this.selectionEnd);
+            this.selectionEnd = s+1; 
+        }
+    }
+}
